81e735d1017985f70681b16ad9417c511fc6a965
kaikane <reznickov.k@yandex.ru>
2024-07-31 14:00:05 +0500

    force dark theme

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,12 +4,13 @@ import { useThemeModeProvider } from '~/store/themeMode.store';
 import { Theme } from '~/store/themeMode.types';
 
 const colorMode = useColorMode();
+colorMode.preference = Theme.Dark;
 const useThemeMode = useThemeModeProvider();
 const { themeState } = storeToRefs(useThemeMode);
 const isDarkTheme = computed(() => themeState.value == Theme.Dark);
 
 </script>
-// TO-DO Создать остальную разметку, докинуть страниц, шапку
+// TO-DO Создать остальную разметку, докинуть страниц, шапку, сохранять выбор темы
 <template>
   <div class="flex justify-between border-b-2 p-5">
     <div class=" font-bold text-xl">
