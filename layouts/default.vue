1add782d733318eaafe92334a81cba80a46d1124
kaikane <reznickov.k@yandex.ru>
2024-08-06 16:06:10 +0500

    todo

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,7 +9,7 @@ const { themeState } = storeToRefs(useThemeMode);
 const isDarkTheme = computed(() => themeState.value == Theme.Dark);
 
 </script>
-// TO-DO Создать остальную разметку, докинуть страниц, шапку, сохранять выбор темы
+<!--TODO: Создать остальную разметку, докинуть страниц, шапку, сохранять выбор темы-->
 <template>
   <div class="flex justify-between border-b-2 p-5">
     <div class=" font-bold text-xl">
