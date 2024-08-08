8383ac107d442c2e75592ac75e82b44f80fa61b6
kaikane <reznickov.k@yandex.ru>
2024-08-08 11:43:47 +0500

    Отделяю в отдельный компонент

---
+++ components/cs/ThemePicker/ThemePicker.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+const colorMode = useColorMode();
+
+const switchColorMode = () => {
+  if (colorMode.preference == 'system') {
+    colorMode.value == 'dark' ? colorMode.preference = 'light' : colorMode.preference = 'dark';
+    colorSwitch.value = !colorSwitch.value;
+  } else {
+    colorMode.preference == 'dark' ? colorMode.preference = 'light' : colorMode.preference = 'dark';
+    colorSwitch.value = !colorSwitch.value;
+  }
+}
+
+
+const isDarkTheme = () => {
+  if (colorMode.preference == 'system') {
+    if (colorMode.value == 'dark') return true
+  } else if (colorMode.preference == 'dark') return true
+  return false
+}
+
+const colorSwitch = ref();
+
+onMounted(() => {
+  colorSwitch.value = isDarkTheme();
+})
+</script>
+
+<template>
+  <div>
+    {{colorMode.value}} {{colorMode.forced}} {{colorMode.unknown}} {{colorMode.preference}}
+    <UiSwitch class="" :checked="colorSwitch"  @update:checked="switchColorMode"></UiSwitch>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
