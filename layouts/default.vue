1ddeca1810dcc975cd71b1abd66456d34d04e995
kaikane <reznickov.k@yandex.ru>
2024-08-09 09:38:50 +0500

    Роутинг и мета

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,28 +7,35 @@ import {
   NavigationMenuTrigger
 } from "~/components/ui/navigation-menu";
 
-const menuItems = ['CV','Проекты','Приколюхи']
+const route = useRoute();
+console.log(route.meta.title)
+
+const menuItems = [{name: 'CV', href: '/cv'},{name: 'Проекты', href: '/projects'},{name: 'Приколюхи', href: '/other'}]
 </script>
 <!--TODO: Создать остальную разметку, докинуть страниц, шапку, пересмотреть централизацию выбора темы-->
 <template>
-  <div class="flex justify-between border-b-2 p-5">
-    <div class=" font-bold text-xl">
-      KaiKane Lending
+  <div class="flex justify-between border-b-2 p-1">
+    <div class="flex justify-between gap-10">
+    <div class=" font-bold text-xl hover:bg-accent p-2 rounded-2xl">
+      <NuxtLink to="/" :class="{'text-blue-500': route.meta.title == 'KaiKane Lendos'}">
+        KaiKane Lending
+      </NuxtLink>
     </div>
-    <NavigationMenu>
-      <NavigationMenuList class="flex gap-3">
-        <NavigationMenuItem v-for="item in menuItems" :key="item" >
-          <NavigationMenuLink as-child>
-            <a href="" class="hover:bg-active focus:bg-accent p-2 rounded-lg bg-gray-800">
-              {{item}}
-            </a>
-
-          </NavigationMenuLink>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
-    <ThemePicker>
-    </ThemePicker>
+      <NavigationMenu>
+        <NavigationMenuList class="flex gap-3">
+          <NavigationMenuItem v-for="item in menuItems" :key="item.href" >
+            <NavigationMenuLink as-child>
+              <a :href="item.href" class="hover:bg-active
+              focus:bg-accent p-2 hover:bg-accent rounded-2xl border-2
+              border-accent" :class="{'border-blue-500': route.meta.title === item.name}">
+                {{item.name}}
+              </a>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
+  <theme-picker class="flex items-center"/>
   </div>
   <div class="">
     <slot />
