760a91ec577b41aafb83b957a1ae5840a38073b0
kaikane <reznickov.k@yandex.ru>
2024-08-23 10:52:55 +0500

    Component created

---
+++ components/cs/text-link/TextLink.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const props = defineProps(
+    {
+      link: String
+    }
+)
+</script>
+
+<template>
+  <NuxtLink class="text-blue-500 hover:text-cyan-500 dark:hover:text-cyan-500" :to="link">
+    <slot></slot>
+  </NuxtLink>
+</template>
