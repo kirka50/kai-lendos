ffddf108dfd0b335ef3f7b0d2bcf72c187922bc3
kaikane <reznickov.k@yandex.ru>
2024-08-28 13:31:30 +0500

    content fill

--- pages/cv.vue
+++ pages/cv.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+
 definePageMeta({
   title: "CV",
 });
@@ -7,10 +8,65 @@ useHead({
 })
 const route = useRoute();
 console.log(route.params)
+
+const date = new Date().getFullYear();
 </script>
 <!--TODO: Сделать резюме, основу взять из того сайта-->
 <template>
-
+  <div class="mx-48">
+    <div class="gap-2 flex-col flex">
+      <div class="rounded-2xl w-fit overflow-hidden">
+        <img src="@/assets/cv/person_image.jpg"  class="h-56 w-56 object-right object-cover" alt="" />
+      </div>
+      <div class="">
+        <div>
+          <p class="text-4xl">Резников Кирилл Сергеевич</p>
+        </div>
+        <div class="flex justify-between mt-2 h-32">
+          <div class="text-xl font-light">
+            <p>Возраст: {{date - 2002}}</p>
+            <p>Место проживания: Екатеринбург</p>
+          </div>
+          <div class="bg-gradient-to-b rounded-2xl bg-accent p-2 w-1/3 h-full">
+            <UiBadge>
+              adad
+            </UiBadge>
+          </div>
+        </div>
+      </div>
+      <div class="flex justify-between">
+        <div>
+          Должность
+        </div>
+        <div>
+          Контактные данные
+        </div>
+      </div>
+      <div class="flex justify-between">
+        Опыт работы
+        <div>
+          Время
+        </div>
+        <div>
+          Данные о работе
+        </div>
+      </div>
+    </div>
+    <UiSeparator/>
+    <div>
+      <div>
+        Портфолио
+      </div>
+      <UiSeparator/>
+      <div>
+        Образование
+      </div>
+      <UiSeparator/>
+      <div>
+        О себе
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
