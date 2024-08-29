1db6e81ebd276b75c4a2223112b2d36500ab69cd
kaikane <reznickov.k@yandex.ru>
2024-08-29 14:02:58 +0500

    content fill

--- pages/cv.vue
+++ pages/cv.vue
@@ -14,12 +14,39 @@ const cvData = ref(
       personData: {
         birthYear: 2002,
         livePlace: "Екатеринбург",
-        skillsTags: [{
+        skillsTags: [
+          {
+            skill: "Front-End Developer",
+          },
+          {
+            skill: "Верстка",
+          },
+            {
           skill: "Js",
-          skillIcon
-        }]
-      }
-    }
+          skillIcon: "devicon:javascript"
+            },
+          {
+            skill: "Vue",
+            skillIcon: "devicon:vuejs"
+          },
+          {
+            skill: "Figma",
+            skillIcon: "devicon:react"
+          },
+        ]
+      },
+      workExp: [{
+        workName: "Фриланс",
+        workDate: "Январь 2023 - Май 2024",
+        workTasks: ['Адаптивно - отзывчивая вёрстка по макетам',
+          'Разработка модулей и компонентов на Vue','Создание веб-страниц с нуля',
+          'Разработка компонентов на React'],
+        taskDesc: 'Верстка лендосов, страниц магазина, создание дизайнов и макетов на figmа'
+      }],
+      wannableJobs:['Frontend разработчик','Js разработчик','Fullstack разработчик']
+
+    },
+
 );
 
 const date = new Date().getFullYear();
@@ -28,8 +55,10 @@ const date = new Date().getFullYear();
 <template>
   <div class="">
     <div class="gap-2 flex-col flex">
-      <div class="rounded-2xl w-fit overflow-hidden">
-        <img src="@/assets/cv/person_image.jpg"  class="h-56 w-56 object-right object-cover" alt="" />
+      <div class="flex flex-col items-center sm:items-center md:items-center xl:items-start lg:items-start">
+        <div class="rounded-2xl w-fit overflow-hidden">
+          <img src="@/assets/cv/person_image.jpg" class="h-56 w-56 object-right object-cover" alt=""/>
+        </div>
       </div>
       <div class="">
         <div>
@@ -40,25 +69,39 @@ const date = new Date().getFullYear();
             <p><strong class="font-bold">Возраст:</strong> {{date - 2002}}</p>
             <p><strong class="font-bold">Место проживания:</strong> Екатеринбург</p>
           </div>
-          <div class="bg-gradient-to-b align-bottom rounded-2xl
-          bg-accent p-2 w-1/3 h-full flex flex-row gap-2">
-            <div class="w-fit">
-              <Icon size="2em" name="twemoji:fire"/>
+          <div class="bg-gradient-to-b justify-evenly rounded-2xl
+          bg-accent p-2 w-1/3 h-full flex flex-row flex-wrap gap-2 items-baseline">
+<!--            <div class="w-fit">
+              <Icon size="2em" name="twemoji:fire"/> Скилы:
+            </div>-->
+            <div v-for="skill in cvData.personData.skillsTags" :key="skill.skill">
+              <Icon v-if="skill?.skillIcon" size="1.5em" :name="skill.skillIcon"></Icon> {{skill.skill}}
             </div>
-            <div>React, Vue, Js,</div>
+
           </div>
         </div>
       </div>
-      <div class="flex justify-between">
+      <div class="flex mt-4 justify-between font-bold text-xl">
         <div>
-          Должность
+          <div>Желаемая должность</div>
+          <ul class="font-light ml-10 list-disc">
+            <li class="mt-4" v-for="job in cvData.wannableJobs">{{job}}</li>
+          </ul>
         </div>
         <div>
           Контактные данные
         </div>
       </div>
+      <div class="flex justify-between font-bold text-xl">
+        <div>
+          Должность
+        </div>
+        <div>
+          Описание
+        </div>
+      </div>
       <div class="flex justify-between">
-        Опыт работы
+        <div>Опыт работы</div>
         <div>
           Время
         </div>
