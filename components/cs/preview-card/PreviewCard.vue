d91e7875e695338fe2242ca63b1cb73affa5638d
kaikane <reznickov.k@yandex.ru>
2024-09-10 15:37:06 +0500

    cv content fill

--- components/cs/preview-card/PreviewCard.vue
+++ components/cs/preview-card/PreviewCard.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
   import type {PreviewCardProps} from "./PreviewCard";
-
   const props = defineProps<PreviewCardProps>()
 
 </script>
@@ -8,7 +7,7 @@
 <template>
   <UiCard class="rounded-2xl overflow-hidden shadow-sm flex flex-col justify-between">
     <UiCardHeader class="p-0 m-0 h-fit">
-      <img :src="`portfolio/${props.image}`" alt="1" class="object-cover h-36 md:h-64 lg:h-80" />
+      <img :src="`./portfolio/${props.image}`" alt="1" class="object-cover h-36 md:h-64 lg:h-80" />
     </UiCardHeader>
     <UiCardFooter class="bg-black p-2 justify-center">
       {{props.title}}
