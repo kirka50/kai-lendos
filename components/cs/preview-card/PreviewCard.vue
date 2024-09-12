63377046c6dfc4fc17765e33e5a32ebb2aa49b02
kaikane <reznickov.k@yandex.ru>
2024-09-12 15:48:43 +0500

    href added

--- components/cs/preview-card/PreviewCard.vue
+++ components/cs/preview-card/PreviewCard.vue
@@ -12,13 +12,15 @@
 
 <template>
   <UiCard class="rounded-2xl overflow-hidden shadow-sm flex flex-col justify-between">
-    <UiCardHeader class="p-0 m-0 h-fit">
-      <img :src="getImageUrl(props.image)" alt="1" class="object-cover h-36 md:h-64 lg:h-80" />
-    </UiCardHeader>
-    <UiCardFooter class="bg-black p-2 justify-center grid">
-      <p>{{ props.title }}</p>
-      <p class="text-xs italic font-extralight">{{props.description}}</p>
-    </UiCardFooter>
+    <a :href="props.href">
+      <UiCardHeader class="p-0 m-0 h-fit">
+        <img :src="getImageUrl(props.image)" alt="1" class="object-cover h-36 md:h-64 lg:h-80"/>
+      </UiCardHeader>
+      <UiCardFooter class="bg-black p-2 justify-center grid">
+        <p>{{ props.title }}</p>
+        <p class="text-xs italic font-extralight">{{ props.description }}</p>
+      </UiCardFooter>
+    </a>
   </UiCard>
 </template>
 
